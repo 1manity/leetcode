a0863a385714d5f6a99359d2bf7c13bd66e89df8
1manity <1052116648@qq.com>
2023-11-08 12:49:06 +0800

    20231108 起床 闲来无事写一题

---
+++ 简单/70. 爬楼梯/index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n === 1 || n === 2) return n
+    let ans = [1,2]
+    for (let i = 3;i<=n;i++) {
+        ans[1] = ans[0] + ans[1]
+        ans[0] = ans[1] - ans[0]
+    }
+    return ans[1]
+};
+console.log(climbStairs(45))
